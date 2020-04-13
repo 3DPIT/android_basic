c029821b51d7bf8f3a048799ff1d6121890f5eec
3DPIT <47946136+3DPIT@users.noreply.github.com>
2020-04-14 01:18:06 +0900

    소스 재조정
    2020.04.14 소스 재조정

--- location/app/src/main/java/com/example/location/MainActivity.java
+++ location/app/src/main/java/com/example/location/MainActivity.java
@@ -19,49 +19,16 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private Button button1;
-    private TextView txtResult;
+    private TextView textView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button1 = (Button)findViewById(R.id.button1);
-        txtResult = (TextView)findViewById(R.id.txtResult);
+        textView1 = (TextView)findViewById(R.id.textView);
 
         final LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if ( Build.VERSION.SDK_INT >= 23 &&
-                ContextCompat.checkSelfPermission( getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
-            ActivityCompat.requestPermissions( MainActivity.this, new String[] {  android.Manifest.permission.ACCESS_FINE_LOCATION  },
-                    0 );
-        }
-        else {
-            Log.d("current_location", "현재 위치 찾기 시작");
-            Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            if (location == null) {
-                location = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-            } else {
-                Log.d("current_location", "111작");
-
-                String provider = location.getProvider();
-                double longitude = location.getLongitude();
-                double latitude = location.getLatitude();
-                double altitude = location.getAltitude();
-
-                txtResult.setText("위치정보 : " + provider + "\n" +
-                        "위도 : " + longitude + "\n" +
-                        "경도 : " + latitude + "\n" +
-                        "고도  : " + altitude);
-
-                lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-                        1000,
-                        1,
-                        gpsLocationListener);
-                lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
-                        1000,
-                        1,
-                        gpsLocationListener);
-            }
-        }
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -76,17 +43,14 @@ public class MainActivity extends AppCompatActivity {
                     if (location == null) {
                         location = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
                     } else {
-
                         String provider = location.getProvider();
                         double longitude = location.getLongitude();
                         double latitude = location.getLatitude();
                         double altitude = location.getAltitude();
-
-                        txtResult.setText("위치정보 : " + provider + "\n" +
-                                "위도 : " + longitude + "\n" +
-                                "경도 : " + latitude + "\n" +
+                        textView1.setText("위치정보 : " + provider + "\n" +
+                                "위도 : " + latitude + "\n" +
+                                "경도 : " + longitude + "\n" +
                                 "고도  : " + altitude);
-
                         lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,
                                 1000,
                                 1,
@@ -109,9 +73,9 @@ public class MainActivity extends AppCompatActivity {
                 double latitude = location.getLatitude();
                 double altitude = location.getAltitude();
 
-                txtResult.setText("위치정보 : " + provider + "\n" +
-                        "위도 : " + longitude + "\n" +
-                        "경도 : " + latitude + "\n" +
+            textView1.setText("위치정보 : " + provider + "\n" +
+                       "위도 : " + latitude + "\n" +
+                        "경도 : " + longitude + "\n" +
                         "고도  : " + altitude);
         }
         public void onStatusChanged(String provider, int status, Bundle extras) {
