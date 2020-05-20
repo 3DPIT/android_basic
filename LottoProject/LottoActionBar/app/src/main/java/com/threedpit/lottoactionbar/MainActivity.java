9008181c0acbfd4aab02e57fbc8c4195ac31d0db
3DPIT <47946136+3DPIT@users.noreply.github.com>
2020-05-20 21:51:38 +0900

    2020.05.20 액션바 추가
    2020.05.20 액션바 추가

--- LottoProject/LottoActionBar/app/src/main/java/com/threedpit/lottoactionbar/MainActivity.java
+++ LottoProject/LottoActionBar/app/src/main/java/com/threedpit/lottoactionbar/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ActionBar ab = getSupportActionBar();
-        
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actionbar_actions, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_btn1:
+                //playBtn();
+                return true;
+            case R.id.action_btn2:
+              //  stopBtn();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
